83de666dffdb5e493140043b94531c3075a61875
sergiolitwiniuk85 <sergiolitwiniuk85@gmail.com>
2024-12-30 10:42:06 -0300

    install update

--- install.sh
+++ install.sh
@@ -4,6 +4,13 @@
 docker pull ezlabgva/busco:v5.8.2_cv1
 docker tag ezlabgva/busco:v5.8.2_cv1 busco:latest
 
+docker pull ncbi/pgap-dev:2024-12-19.build5922
+docker tag ncbi/pgap-dev:2024-12-19.build5922 pgap:latest
 
 # Build the Docker Compose services
 docker-compose build
+
+
+
+#download pgap
+curl -OL https://raw.githubusercontent.com/ncbi/pgap/refs/heads/master/scripts/pgap.py && chmod u+x pgap.py && ./pgap.py --update
